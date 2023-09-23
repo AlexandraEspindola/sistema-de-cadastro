2c382749399bf06b8dfdb623fc8f3790090759b0
alexandraespindola <ale.espindola.silva@gmail.com>
2023-09-23 18:16:47 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("O usuário colaborador fez esse commit sem você ver!")
+console.log("Enviando para o Github")
